079ad6d28cdb7d4229d718061b06d0739de72717
ArtemOsypov <artem.osypov@gmail.com>
2023-03-18 00:05:27 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,28 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-const onPlay = function (data) {
-  // data is an object containing properties specific to that event
-};
+const SAVED_TIME = 'videoplayer-current-time';
 
-player.on('play', onPlay);
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
 
-player.on('eventName', function (data) {
-  // data is an object containing properties specific to that event
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(SAVED_TIME, JSON.stringify(data));
+  }, 500)
+);
 
-localStorage.setItem('videoplayer-current-time', '');
+// localStorage.setItem(SAVED_TIME, '');
+
+// player
+//   .getCurrentTime()
+//   .then(function (seconds) {
+//     // seconds = the current playback position
+//   })
+//   .catch(function (error) {
+//     // an error occurred
+//   });
