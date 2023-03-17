7ab86643b2ff10c5016de56544c7d08d3be78ede
ArtemOsypov <artem.osypov@gmail.com>
2023-03-18 00:29:04 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,15 +5,10 @@ const player = new Player(iframe);
 
 const SAVED_TIME = 'videoplayer-current-time';
 
-// const onPlay = function (data) {
-//   // data is an object containing properties specific to that event
-// };
-
 player.on(
   'timeupdate',
   throttle(function (data) {
     localStorage.setItem(SAVED_TIME, JSON.stringify(data));
-    console.log(data);
   }, 500)
 );
 const currentStopTime = localStorage.getItem(SAVED_TIME);
@@ -21,11 +16,7 @@ const currentTime = JSON.parse(currentStopTime);
 
 player
   .getCurrentTime(currentTime.seconds)
-  .then(function (seconds) {
-    // seconds = the current playback position
-  })
-  .catch(function (error) {
-    // an error occurred
-  });
+  .then(function (seconds) {})
+  .catch(function (error) {});
 
 player.setCurrentTime(currentTime.seconds || 0);
